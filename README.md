342907aaf83acd99bc2fc32021929808f1c6fb5c
Guilherme Rodrigues <gadr90@gmail.com>
2019-05-04 16:18:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Render Root Path Feature Example
+
+Reference app for root path feature implementation. This shows how a CDN should perform host and path rewriting.
+
+## How root path support works in Render
+
+If your main domain is served by a different platform or system and you want to scope a VTEX IO store in a fixed path prefix, you can use the Root Path feature.
+
+For example, given a domain `mybrand.com`, if you want to serve your store under `mybrand.com/ar`, you must configure your CDN to:
+
+1 - Create a public domain for your account and add it to License Manager, e.g. `mybrand.com.ar`.
+2 - For all requests to `mybrand.com/ar`:
+  2.1 - Rewrite the `Host` header to `mybrand.com.ar`.
+  2.2 - Rewrite the request `path` removing the prefix. E.g. `/ar/my-product/p` becomes `/my-product/p`.
+  2.3 - Add a `x-vtex-root-path` header to the request with the value of the removed prefix, e.g. `/ar`.
+
+This will inform Render that all generated links in this site should be prefixed by the root path, in this case `/ar`.
+
+## Example proxy app
+
+This is an example app that performs the rewrites and adds the header for test purposes.
---
+++ node/index.ts
@@ -0,0 +1,47 @@
+import { Service, ServiceContext } from '@vtex/api'
+import { request } from 'http'
+import { pick } from 'ramda'
+
+const rootPath = '/ar'
+
+const pickedHeaders = ['accept-encoding', 'accept-language', 'accept', 'cookie']
+
+export default new Service({
+  routes: {
+    rewrite: async (ctx: ServiceContext) => {
+      const {req, vtex: { account, operationId }} = ctx
+      const host = `${account}.myvtex.com`
+
+      const options = {
+        headers: {
+          ...pick(pickedHeaders, req.headers),
+          host,
+          'x-vtex-operation-id': operationId,
+          'x-vtex-root-path': rootPath,
+        },
+        hostname: host,
+        method: req.method,
+        path: req.url!.replace(rootPath, '/'),
+      }
+
+      console.log('Proxying request:', options)
+
+      await new Promise((resolve, reject) => {
+        const proxyReq = request(options, (res) => {
+          console.log(`Response status: ${res.statusCode}`, res.headers)
+          ctx.status = res.statusCode as number
+          ctx.set(res.headers as any)
+          ctx.body = res
+          resolve()
+        })
+
+        proxyReq.on('error', reject)
+        proxyReq.end()
+
+        // Don't cache errors if request fails
+        ctx.set('cache-control', 'private, no-cache')
+      })
+
+    },
+  },
+})
